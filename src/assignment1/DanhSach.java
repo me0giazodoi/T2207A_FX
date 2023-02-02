8a14fab7074e57ad8cc502345c099f9d1dbe7df5
me0giazodoi <110475358+me0giazodoi@users.noreply.github.com>
2023-02-02 17:20:42 +0700

    update

---
+++ src/assignment1/DanhSach.java
@@ -0,0 +1,15 @@
+package assignment1;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class DanhSach {
+    public TextField txtNguoiNhan;
+    public TextField txtTieuDe;
+    public TextArea txtNoiDung;
+
+    public void Submit(ActionEvent actionEvent) {
+
+    }
+}
