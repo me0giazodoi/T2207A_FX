2ef21d5bd08b3c55a19ae72360f287e0e722c48f
me0giazodoi <110475358+me0giazodoi@users.noreply.github.com>
2023-02-06 14:11:58 +0700

    update

--- src/ThongTin.java
+++ src/ThongTin.java
@@ -2,12 +2,14 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
+import java.util.ArrayList;
+
 public class ThongTin {
     public TextField txtFullName;
     public TextField txtEmail;
     public Text txtInfo;
 
-
+    public ArrayList<Student> ListStudent = new ArrayList<>();
     public void submit(ActionEvent actionEvent) {
         String fn = txtFullName.getText();
         String em = txtEmail.getText();
@@ -15,5 +17,19 @@ public class ThongTin {
         txt += "\n=================";
         txt += "\nFullname: "+fn+"\n"+"Email: "+em;
         txtInfo.setText(txt);
+
+//        // lambda expression
+//        IStudent s1 = (a)->{
+//
+//        };
+//        s1.learn(5);
+//        ArrayList<String> arrs = new ArrayList<>();
+//        arrs.add("Hello");
+//        arrs.add("world");
+//
+//        arrs.stream().filter(s->!s.equals("a")).forEach(s->{
+//            System.out.println(s);
+//        });
+
     }
 }
