3a2be236c29a9a4e855a2311d66d788bd8c745df
me0giazodoi <110475358+me0giazodoi@users.noreply.github.com>
2023-02-13 17:02:53 +0700

    update

---
+++ src/assignment2/PhoneBook.java
@@ -0,0 +1,72 @@
+package assignment2;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PhoneBook implements Initializable {
+
+    public TextField txtName;
+    public TextField txtPhone;
+    public static TextField stName;
+    public static TextField stPhone;
+
+    public ObservableList<PhoneNumber> listPhoneNumber = FXCollections.observableArrayList();
+
+    public static PhoneNumber editPhoneNumber;
+    public TableView<PhoneNumber> tbview;
+    public TableColumn<PhoneNumber, String> cName;
+    public TableColumn<PhoneNumber, String> cPhone;
+    public TableColumn<PhoneNumber, Button> cAction;
+
+
+    public void submit(ActionEvent actionEvent) {
+        String n = txtName.getText();
+        String p = txtPhone.getText();
+
+        if (editPhoneNumber == null) {
+            PhoneNumber pn = new PhoneNumber(n, p);
+            listPhoneNumber.add(pn);
+        } else {
+            for (PhoneNumber pn1 : listPhoneNumber) {
+                if (pn1.name.equals(editPhoneNumber.name) && pn1.phone.equals(editPhoneNumber.phone)) {
+                    pn1.setName(n);
+                    pn1.setPhone(p);
+                }
+            }
+        }
+        tbview.setItems(listPhoneNumber);
+        tbview.refresh();
+        editPhoneNumber = null;
+        clearInput();
+
+    }
+
+    public void clearInput() {
+        this.txtName.clear();
+        this.txtPhone.clear();
+    }
+
+    public void editPhone(MouseEvent mouseEvent) {
+//        editPhoneNumber = lv.getSelectionModel().getSelectedItem();
+        this.txtName.setText(editPhoneNumber.getName());
+        this.txtPhone.setText(editPhoneNumber.getPhone());
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        cName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        cPhone.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        cAction.setCellValueFactory(new PropertyValueFactory<>("edit"));
+
+        stName = txtName;
+        stPhone = txtPhone;
+    }
+}
---
+++ src/assignment2/PhoneNumber.java
@@ -0,0 +1,52 @@
+package assignment2;
+
+import javafx.scene.control.Button;
+
+public class PhoneNumber {
+    String name;
+    String phone;
+
+    Button edit;
+
+    public PhoneNumber(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+        this.edit = new Button("Edit");
+        this.edit.setOnAction((event) -> {
+            PhoneBook.editPhoneNumber = this;
+            PhoneBook.stName.setText(this.name);
+            PhoneBook.stPhone.setText(this.phone);
+        });
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public PhoneNumber setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public PhoneNumber setPhone(String phone) {
+        this.phone = phone;
+        return this;
+    }
+
+    public Button getEdit() {
+        return edit;
+    }
+
+    public PhoneNumber setEdit(Button edit) {
+        this.edit = edit;
+        return this;
+    }
+
+    public String toString() {
+        return this.name;
+    }
+}
