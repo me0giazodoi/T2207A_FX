7abaae5f919bc8eff823a67cbf787fffe6646042
me0giazodoi <110475358+me0giazodoi@users.noreply.github.com>
2023-02-27 16:30:53 +0700

    update

--- src/demo1/MainThread.java
+++ src/demo1/MainThread.java
@@ -4,6 +4,25 @@ public class MainThread {
     public static void main(String[] args){
         Sub1 s1 = new Sub1(5,30);
         s1.start();
+
+        Sub2 s2 = new Sub2();
+        Thread t = new Thread(s2);
+        t.start();
+
+        Runnable r = ()->{
+            for (int i=0;i<20;i++){
+                System.out.println();
+            }
+        };
+
+        Thread t2 = new Thread(r);
+        t2.start();
+
+        // tạo thread chạy luôn mà không cần khai báo biến
+        new Thread(()->{
+            // cần chạy gì viết vào đây
+        }).start();
+
 //        for (int i=0;i<20;i++){
 //            System.out.println("Main i="+i);
 //            try{
---
+++ src/demo1/Sub2.java
@@ -0,0 +1,15 @@
+package demo1;
+
+public class Sub2 implements Runnable{
+    @Override
+    public void run() {
+        for (int i=0;i<20;i++){
+            System.out.println("Sub 2  i ="+i);
+            try{
+                Thread.sleep(1000);
+            }catch (Exception e){
+
+            }
+        }
+    }
+}
